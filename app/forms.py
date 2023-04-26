905aa403b4da4174d63afd14c0c83574ae1febbd
minnelamohanakumari <minnelamohanakumari18gmail.com>
2023-04-26 11:41:02 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,18 @@ class StudentForm(forms.Form):
     name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
     age=forms.IntegerField()
     email=forms.EmailField()
+    re_enter_email=forms.EmailField()
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('not matched')
+
+
+    def clean_botcatcher(self):
+        b=self.cleaned_data['botcatcher']
+        if len(b)>0:
+            raise forms.ValidationError('not correct')
+
